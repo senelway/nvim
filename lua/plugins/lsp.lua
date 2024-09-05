8c80ad2fdf79e583c7019c798204704abb3a8c99
senelway <senelway@gmail.com>
2024-09-05 13:31:03 +0100

    upd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -203,7 +203,7 @@ return { -- LSP Configuration & Plugins
           local server = servers[server_name] or {}
           -- This handles overriding only values explicitly passed
           -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off)
+          -- certain features of an LSP
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
           require('lspconfig')[server_name].setup(server)
         end,
