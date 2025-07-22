15b3e7842daf1c1fc46845177597e005b8198a83
Dmitrii Strelkov <senelway@gmail.com>
2025-07-22 19:43:42 +0100

    feat: update deps and etc (#11)

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -4,7 +4,7 @@ return {
     -- Creates a beautiful debugger UI
     "rcarriga/nvim-dap-ui",
     -- Installs the debug adapters for you
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     "jay-babu/mason-nvim-dap.nvim",
     -- Add your own debuggers here
     "leoluz/nvim-dap-go",
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,7 +38,7 @@ return {
     local builtin = require("telescope.builtin")
 
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+    -- vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
     vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
     -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
@@ -70,9 +70,7 @@ return {
     end, { desc = "[S]earch [/] in Open Files" })
 
     vim.keymap.set("n", "<leader>sp", function()
-      builtin.find_files({
-        search_dirs = { "~/Projects/" },
-      })
+      builtin.find_files({ search_dirs = { "~/Projects/" } })
     end, { desc = "[S]earch [/] in Open Files" })
 
     -- Shortcut for searching your neovim configuration files
