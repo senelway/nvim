e117de64ca658e80397d59f7cf14456884e1c4e8
Dmitrii Strelkov <senelway@gmail.com>
2025-12-13 19:54:05 +0000

    feat(treesitter): move to main, change neotest go (#13)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -187,6 +187,8 @@ return { -- LSP Configuration & Plugins
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
+      'tree-sitter-cli',
+
       -- formatter
       'stylua',
       -- -- go
