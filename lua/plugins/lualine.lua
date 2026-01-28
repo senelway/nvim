179c8bbe8f06685fe9e565221dcc53f91a351393
senelway <senelway@gmail.com>
2026-01-28 23:52:58 +0100

    update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,52 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-mini/mini.icons', 'folke/trouble.nvim', 'rcarriga/nvim-dap-ui' },
-  opts = {
-    extensions = { 'trouble', 'nvim-dap-ui' },
-    options = {
-      component_separators = '|',
-    },
-    sections = {
-      lualine_c = { { 'filename', path = 1 } },
-    },
-  },
+  config = function()
+    local C = require('catppuccin.palettes').get_palette()
+
+    local bg = C.mantle -- Options: C.crust, C.mantle, C.base, C.surface0
+
+    vim.cmd.highlight('MsgArea guibg=' .. bg)
+    local theme = {
+      normal = {
+        a = { bg = bg, fg = C.blue, gui = 'bold' },
+        b = { bg = bg, fg = C.blue },
+        c = { bg = bg, fg = C.text },
+      },
+      insert = {
+        a = { bg = bg, fg = C.green, gui = 'bold' },
+        b = { bg = bg, fg = C.green },
+      },
+      command = {
+        a = { bg = bg, fg = C.peach, gui = 'bold' },
+        b = { bg = bg, fg = C.peach },
+      },
+      visual = {
+        a = { bg = bg, fg = C.mauve, gui = 'bold' },
+        b = { bg = bg, fg = C.mauve },
+      },
+      replace = {
+        a = { bg = bg, fg = C.red, gui = 'bold' },
+        b = { bg = bg, fg = C.red },
+      },
+      inactive = {
+        a = { bg = bg, fg = C.surface1 },
+        b = { bg = bg, fg = C.surface1 },
+        c = { bg = bg, fg = C.overlay0 },
+      },
+    }
+
+    require('lualine').setup {
+      extensions = { 'trouble', 'nvim-dap-ui' },
+      options = {
+        theme = theme,
+        component_separators = '|',
+        section_separators = '|',
+      },
+      sections = {
+        lualine_a = {},
+        lualine_c = { { 'filename', path = 1 } },
+      },
+    }
+  end,
 }
