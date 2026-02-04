d438ed532f576767cc54555cf3cbae9cf6b31f48
senelway <senelway@gmail.com>
2026-02-04 15:04:35 +0100

    fix(git): fix git plugins

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,41 +1,42 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Git Status' })
 
 return {
-  'lewis6991/gitsigns.nvim',
-  event = { 'BufReadPost', 'BufNewFile' },
-  opts = {
-    on_attach = function(bufnr)
-      local gs = require 'gitsigns'
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { 'BufReadPost', 'BufNewFile' },
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
 
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
-      end
-
-      -- Navigation
-      map('n', ']c', function()
-        if vim.wo.diff then
-          vim.cmd.normal { ']c', bang = true }
-        else
-          gs.nav_hunk 'next'
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
         end
-      end)
 
-      map('n', '[c', function()
-        if vim.wo.diff then
-          vim.cmd.normal { '[c', bang = true }
-        else
-          gs.nav_hunk 'prev'
-        end
-      end)
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk 'next'
+          end
+        end)
 
-      -- Preview hunk
-      map('n', '<leader>gd', gs.preview_hunk_inline, { desc = 'Preview hunk inline' })
-    end,
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk 'prev'
+          end
+        end)
+
+        -- Preview hunk
+        map('n', '<leader>gd', gitsigns.preview_hunk_inline, { desc = 'Preview hunk inline' })
+      end,
+    },
   },
   {
     'tpope/vim-fugitive',
-    cmd = { 'Git' },
   },
 }
