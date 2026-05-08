f9a1fc79b860bd89f4281939682fe6d467620be6
Dmitrii Strelkov <senelway@gmail.com>
2026-05-08 17:01:33 +0800

    feat: roslyn (#26)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,9 @@ return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   build = ':TSUpdate',
+  branch = 'main',
   config = function()
-    local wanted = {
+    require('nvim-treesitter').install({
       'go',
       'lua',
       'tsx',
@@ -13,24 +14,23 @@ return {
       'scss',
       'sql',
       'markdown',
+      'markdown_inline',
       'json',
       'http',
       'c_sharp',
       'svelte',
-    }
-    local installed = require('nvim-treesitter.config').get_installed()
-    local todo = vim
-      .iter(wanted)
-      :filter(function(p)
-        return not vim.tbl_contains(installed, p)
-      end)
-      :totable()
-    require('nvim-treesitter').install(todo)
+    })
 
     vim.api.nvim_create_autocmd('FileType', {
-      callback = function()
-        pcall(vim.treesitter.start)
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      callback = function(args)
+        local language = vim.treesitter.language.get_lang(args.match)
+        if not language or not vim.treesitter.language.add(language) then
+          return
+        end
+        vim.treesitter.start(args.buf, language)
+        if vim.treesitter.query.get(language, 'indents') ~= nil then
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
       end,
     })
   end,
